03cceac48e3f89130a14c7c8d386c15ba8bb8f45
Kevin Renault <kevin.renault.92@gmail.com>
2026-01-30 12:49:40 +0100

    feat: refactor front structure with modular features directory and move home component accordingly

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './features/home/home.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
@@ -10,4 +10,4 @@ const routes: Routes = [{ path: '', component: HomeComponent }];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
