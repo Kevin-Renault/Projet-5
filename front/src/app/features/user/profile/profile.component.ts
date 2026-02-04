dee84bf1e9b3d9d0d2deb732c3935cbfebecdc28
Kevin Renault <kevin.renault.92@gmail.com>
2026-02-04 21:10:09 +0100

    feat: connect profil component to services

--- front/src/app/features/user/profile/profile.component.ts
+++ front/src/app/features/user/profile/profile.component.ts
@@ -1,13 +1,19 @@
-import { SlicePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { SlicePipe, AsyncPipe } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { combineLatest, map, Observable } from 'rxjs';
+import { Subscription } from 'src/app/core/models/subscription.model';
+import { Topic } from 'src/app/core/models/topic.model';
+import { SUBSCRIPTION_DATASOURCE, SubscriptionDatasource } from 'src/app/core/services/subscription-datasource.interface';
+import { TOPIC_DATASOURCE, TopicDataSource } from 'src/app/core/services/topic-datasource.interface';
 import { FormElement, DynamicFormComponent } from 'src/app/shared/form/dynamic-form.component';
 
 @Component({
   selector: 'app-profile',
-  imports: [DynamicFormComponent, FormsModule, SlicePipe],
+  imports: [DynamicFormComponent, FormsModule, SlicePipe, AsyncPipe],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
   profileFormElements: FormElement[] = [
@@ -22,25 +28,52 @@ export class ProfileComponent {
     }
   ];
 
-  topics = [
 
-    {
-      title: 'Topic 2',
-      content: 'Content of Topic 2...',
-      subscribed: true
-    },
-    {
-      title: 'Topic 3',
-      content: 'Content of Topic 3...',
-      subscribed: true
-    }
-  ];
 
   onFormSubmit(values: any) {
     alert('Form submitted: ' + JSON.stringify(values, null, 2));
   }
 
-  public toggleSubscription(topic: any) {
-    topic.subscribed = !topic.subscribed;
+  userId: number;
+  myTopics$: Observable<Topic[]> | null = null;
+
+  constructor(
+    @Inject(TOPIC_DATASOURCE) private readonly topicDataSource: TopicDataSource,
+    @Inject(SUBSCRIPTION_DATASOURCE) private readonly subscriptionDataSource: SubscriptionDatasource,
+    private readonly router: Router
+  ) {
+
+    this.userId = 1; // TODO: Remplacer par l'ID de l'utilisateur connecté
+    this.myTopics$ = combineLatest([
+      this.topicDataSource.getAll(),
+      this.subscriptionDataSource.getUserSubscriptions(this.userId)
+    ]).pipe(
+      map(([topics, subs]) => {
+        const abonneIds = new Set(subs.map(s => s.topicId));
+        return topics.filter(topic => abonneIds.has(topic.id));
+      })
+    );
+  }
+
+
+
+  public toggleSubscription(topicId: number) {
+    this.subscriptionDataSource.unsubscribeFromTopic(this.userId, topicId).subscribe(() => {
+      this.refreshSubscriptions();
+    });
+  }
+
+  private refreshSubscriptions() {
+
+    // Met à jour la liste des topics auxquels l'utilisateur est abonné
+    this.myTopics$ = combineLatest([
+      this.topicDataSource.getAll(),
+      this.subscriptionDataSource.getUserSubscriptions(this.userId)
+    ]).pipe(
+      map(([topics, subs]) => {
+        const abonneIds = new Set(subs.map(s => s.topicId));
+        return topics.filter(topic => abonneIds.has(topic.id));
+      })
+    );
   }
 }
