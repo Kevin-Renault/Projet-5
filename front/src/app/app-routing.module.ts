d700bdfecb0a9a8f3a780f3499718f7d2c4e78b6
Kevin Renault <kevin.renault.92@gmail.com>
2026-02-04 08:41:56 +0100

    feat(article): add profile page

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -6,5 +6,8 @@ export const routes: Routes = [
   { path: 'user/register', loadComponent: () => import('./features/user/register/register.component').then(m => m.RegisterComponent) },
   { path: 'user/login', loadComponent: () => import('./features/user/login/login.component').then(m => m.LoginComponent) },
   { path: 'articles', loadComponent: () => import('./features/article/article-list.component').then(m => m.ArticleListComponent) },
-  { path: 'topics', loadComponent: () => import('./features/topic/topic.component').then(m => m.TopicComponent) }
+  { path: 'topics', loadComponent: () => import('./features/topic/topic.component').then(m => m.TopicComponent) },
+  { path: 'articles/create', loadComponent: () => import('./features/article/article-create.component').then(m => m.ArticleCreateComponent) },
+  { path: 'articles/comment', loadComponent: () => import('./features/article/article-comment.component').then(m => m.ArticleCommentComponent) },
+  { path: 'user/profile', loadComponent: () => import('./features/user/profile/profile.component').then(m => m.ProfileComponent) }
 ];
--- front/src/app/features/user/profile/profile.component.ts
+++ front/src/app/features/user/profile/profile.component.ts
@@ -1,9 +1,11 @@
+import { SlicePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { FormElement, DynamicFormComponent } from 'src/app/shared/form/dynamic-form.component';
 
 @Component({
   selector: 'app-profile',
-  imports: [DynamicFormComponent],
+  imports: [DynamicFormComponent, FormsModule, SlicePipe],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
@@ -19,7 +21,26 @@ export class ProfileComponent {
       pattern: '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#?!@$%^&*-]).{8,}$'
     }
   ];
+
+  topics = [
+
+    {
+      title: 'Topic 2',
+      content: 'Content of Topic 2...',
+      subscribed: true
+    },
+    {
+      title: 'Topic 3',
+      content: 'Content of Topic 3...',
+      subscribed: true
+    }
+  ];
+
   onFormSubmit(values: any) {
     alert('Form submitted: ' + JSON.stringify(values, null, 2));
+  }
+
+  public toggleSubscription(topic: any) {
+    topic.subscribed = !topic.subscribed;
   }
 }
