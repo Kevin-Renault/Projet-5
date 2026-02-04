8df408ac47e910360293bddbdedf36892ce2ca97
Kevin Renault <kevin.renault.92@gmail.com>
2026-02-04 21:06:45 +0100

    feat: connect article create component to article service

--- front/src/app/features/article/article-create.component.ts
+++ front/src/app/features/article/article-create.component.ts
@@ -1,28 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { Article } from 'src/app/core/models/article.model';
+import { Topic } from 'src/app/core/models/topic.model';
+import { ARTICLE_DATASOURCE, ArticleDataSource } from 'src/app/core/services/article-datasource.interface';
+import { TOPIC_DATASOURCE, TopicDataSource } from 'src/app/core/services/topic-datasource.interface';
 import { FormElement, DynamicFormComponent } from 'src/app/shared/form/dynamic-form.component';
 
 @Component({
   selector: 'app-article-create',
   imports: [DynamicFormComponent],
   templateUrl: './article-create.component.html',
-  styleUrl: './article-create.component.scss'
+  styleUrls: ['./article-create.component.scss']
 })
-export class ArticleCreateComponent {
-
+export class ArticleCreateComponent implements OnDestroy {
   articleFormElements: FormElement[] = [
     {
-      type: 'select', name: 'thème sélectionné', placeholder: 'Selectionner un thème', required: true, options: [
-        { value: 'Java', label: 'Java' },
-        { value: 'Angular', label: 'Angular' },
-        { value: 'DevOps', label: 'DevOps' }
-      ]
+      type: 'select', name: 'topicId', placeholder: 'Sélectionner un thème', required: true, options: []
     },
-    { type: 'text', name: 'Titre de l\'article', placeholder: 'Titre de l\'article', required: true },
-    { type: 'textarea', name: 'Contenu de l\'article', placeholder: 'Contenu de l\'article', required: true }
+    { type: 'text', name: 'title', placeholder: 'Titre de l\'article', required: true },
+    { type: 'textarea', name: 'content', placeholder: 'Contenu de l\'article', required: true }
   ];
 
-  onFormSubmit(values: any) {
-    alert('Form submitted: ' + JSON.stringify(values, null, 2));
+
+  constructor(@Inject(TOPIC_DATASOURCE) private readonly topicDataSource: TopicDataSource,
+    @Inject(ARTICLE_DATASOURCE) private readonly articleDataSource: ArticleDataSource,
+    private readonly router: Router) {
+    this.topicDataSource.getAll().subscribe((topics: Topic[]) => {
+      this.articleFormElements[0].options = topics.map(t => ({
+        value: t.id + "",
+        label: t.name
+      }));
+    });
   }
 
+  private readonly destroy$ = new Subject<void>();
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onFormSubmit(values: Partial<Article>): void {
+
+    values.createdAt = new Date().toISOString();
+    values.updatedAt = new Date().toISOString();
+    //TODO: Remplacer par l'ID de l'utilisateur connecté
+    values.authorId = 1; // Utilisateur fictif
+    this.articleDataSource.create(values as Article).pipe(
+      takeUntil(this.destroy$)
+    ).subscribe({
+      next: () => { this.router.navigate(['/articles']); },
+      error: () => alert('Erreur lors de la création de l\'article'),
+    });
+  }
 }
