f6a0fc48042bcac1a305883afe08af704fb1b7b2
Kevin Renault <kevin.renault.92@gmail.com>
2026-02-05 10:46:47 +0100

    feat: add AuthGuard to protected routes in app routing module

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,13 +1,29 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent) },
   { path: 'test', loadComponent: () => import('./features/header-test.component').then(m => m.HeaderTestComponent) },
   { path: 'user/register', loadComponent: () => import('./features/user/register/register.component').then(m => m.RegisterComponent) },
   { path: 'user/login', loadComponent: () => import('./features/user/login/login.component').then(m => m.LoginComponent) },
-  { path: 'articles', loadComponent: () => import('./features/article/article-list.component').then(m => m.ArticleListComponent) },
-  { path: 'topics', loadComponent: () => import('./features/topic/topic.component').then(m => m.TopicComponent) },
-  { path: 'articles/create', loadComponent: () => import('./features/article/article-create.component').then(m => m.ArticleCreateComponent) },
-  { path: 'articles/:id/comment', loadComponent: () => import('./features/article/article-comment.component').then(m => m.ArticleCommentComponent) },
-  { path: 'user/profile', loadComponent: () => import('./features/user/profile/profile.component').then(m => m.ProfileComponent) }
+  {
+    path: 'articles', loadComponent: () => import('./features/article/article-list.component').then(m => m.ArticleListComponent),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'topics', loadComponent: () => import('./features/topic/topic.component').then(m => m.TopicComponent),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'articles/create', loadComponent: () => import('./features/article/article-create.component').then(m => m.ArticleCreateComponent),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'articles/:id/comment', loadComponent: () => import('./features/article/article-comment.component').then(m => m.ArticleCommentComponent),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'user/profile', loadComponent: () => import('./features/user/profile/profile.component').then(m => m.ProfileComponent),
+    canActivate: [AuthGuard]
+  }
 ];
