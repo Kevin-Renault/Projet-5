5a2d36cabf8df4c186f2da6f06da3955471cc471
Kevin Renault <kevin.renault.92@gmail.com>
2026-02-20 12:19:49 +0100

    ci: add Cypress E2E code coverage like Jest

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "cypress";
+import codeCoverageTask from "@cypress/code-coverage/task";
 
 export default defineConfig({
   allowCypressEnv: false,
@@ -8,7 +9,8 @@ export default defineConfig({
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.ts",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      codeCoverageTask(on, config);
+      return config;
     },
   },
 });
